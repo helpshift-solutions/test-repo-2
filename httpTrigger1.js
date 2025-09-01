27f0948d2d4d0ffdd48799fe1ba5fb85ecbba7a9
aashigarg-HS <aashi.garg@helpshift.com>
2025-09-01 12:17:04 +0530

    changes made

--- httpTrigger1.js
+++ httpTrigger1.js
@@ -5,7 +5,6 @@ app.http('httpTrigger1', {
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
-co
         const name = request.query.get('name') || await request.text() || 'world';
 
         return { body: `Hello, ${name}!` };
